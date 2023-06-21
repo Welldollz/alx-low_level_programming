a7137c745e6827a56f370e84a72f1ac4c5111eb8
Welldollz <dollywelile000@gmail.com>
2023-06-20 20:09:47 -0700

    4-isalpha

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,8 +8,10 @@
  * -1 and prints - if n is less than zero
  */
 int print_sign(int n) {
-  if (n > 0) {
-    _putchar('+');
+  
+	if (n > 0)
+  {
+    	_putchar('+');
     return (1);
   } else if (n == 0) {
     _putchar(48);
